5cfcdb9db570d9c32d7e0af79cb39f3ab9cc00e0
rishu-rgb <rishuraj7662.m@gmail.com>
2026-06-22 02:23:26 +0530

    Update student.service.js

--- backend/src/modules/student/student.service.js
+++ backend/src/modules/student/student.service.js
@@ -1,11 +1,1 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/db');
 
-const PORT = process.env.PORT || 5001;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-});
